c83eaa795f73bd27488bdddc4cefc1900b7c81e0
wkdwjdgns0920 <wkdwjdgns0920@gmail.com>
2024-08-14 12:30:33 +0900

    DI 프레임워크 만들기

---
+++ src/main/java/org/example/di/BeanFactory.java
@@ -0,0 +1,69 @@
+package org.example.di;
+
+import org.example.annotation.Inject;
+import org.reflections.ReflectionUtils;
+import org.reflections.Reflections;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+public class BeanFactory {
+
+    private final Set<Class<?>> preInstantiatedClazz;
+    private Map<Class<?>, Object> beans = new HashMap<>();
+
+    public BeanFactory(Set<Class<?>> preInstantiatedClazz) {
+        this.preInstantiatedClazz = preInstantiatedClazz;
+        initialize();
+    }
+
+    private void initialize() {
+        for (Class<?> clazz : preInstantiatedClazz) {
+            Object instance = createInstance(clazz);
+            beans.put(clazz, instance);
+        }
+    }
+
+    // 1.UserController
+    // 3.UserService
+    private Object createInstance(Class<?> clazz) {
+        // 생성자
+        Constructor<?> constructor = findConstructor(clazz);
+        // 파라미터
+        List<Object> parameters = new ArrayList<>();
+        for (Class<?> typeClass : constructor.getParameterTypes()) {
+            // 2.UserService
+            parameters.add(getParameterByClass(typeClass));
+        }
+
+        // 인스턴스 생성
+        try {
+            return constructor.newInstance(parameters.toArray());
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Constructor<?> findConstructor(Class<?> clazz) {
+        Constructor<?> constructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+        if (Objects.nonNull(constructor)) {
+            return constructor;
+        }
+
+        return clazz.getConstructors()[0];
+    }
+
+    private Object getParameterByClass(Class<?> typeClass) {
+        Object instanceBean = getBean(typeClass);
+
+        if(Objects.nonNull(instanceBean)) {
+            return instanceBean;
+        }
+        return createInstance(typeClass);
+    }
+
+    public <T> T getBean(Class<T> requiredType) {
+        return (T) beans.get(requiredType);
+    }
+}
---
+++ src/main/java/org/example/service/UserService.java
@@ -0,0 +1,9 @@
+package org.example.service;
+
+import org.example.annotation.Controller;
+import org.example.annotation.Inject;
+
+@Controller
+public class UserService {
+
+}
